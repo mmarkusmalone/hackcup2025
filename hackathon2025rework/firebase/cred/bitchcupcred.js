5d27ba6bb8490d7ede425da3fad96ac9ebb2a27a
Maya Markus-Malone <mayamarkus-malone@Mayas-MacBook-Pro-9.local>
2025-02-09 10:48:49 -0500

    deleted

--- hackathon2025rework/firebase/cred/bitchcupcred.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyAnBQsZQd1hgZw8Vxzu-tdjHzKE2cxQShs",
-    authDomain: "bitchcup.firebaseapp.com",
-    projectId: "bitchcup",
-    storageBucket: "bitchcup.firebasestorage.app",
-    messagingSenderId: "369068773771",
-    appId: "1:369068773771:web:a678b40c7fd857e74a5f8e",
-    measurementId: "G-GGJ0SFVPCQ"
-  };
